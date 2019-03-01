1a7b9cfc6f32ae7c76ee2f005a0eae7c57a77edf
Grod24 <Greg_rodriguez@Live.com>
2019-02-28 23:19:02 -0500

    updating to master

---
+++ student/index.php
@@ -0,0 +1,48 @@
+<?php
+//======================================================================
+// LOGIN PAGE
+//======================================================================
+  /* Quick Paths */
+  include_once (realpath(dirname(__FILE__).'/php/path.php'));
+
+  /* Page Name */
+  $page_name = "home";
+
+  /* Start The Session */
+  session_start(); 
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+  <?php include_once (ROOT_PATH . '/include/head.php'); ?>
+  </head>
+  <body class="<?php echo $page_name; ?>">
+  <?php include_once (ROOT_PATH . '/include/header.php'); ?>
+    <main role="main" class="container">
+      <div class="row justify-content-sm-center">
+        <div class="col-sm-4">
+          <h1>Login</h1>
+          <form action="<?php echo BASE_URL; ?>/php/authenticate.php" method="post">
+            <div class="form-group">
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="username" placeholder="Enter username" name="username" required>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password" name="password" required>
+            </div>
+            <?php
+              /* Error Message */
+              if (isset($error)) {
+                // uses bootstrap alert style for error messages
+                echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+              }
+            ?>
+            <button type="submit" class="btn btn-primary">Log In</button>
+          </form>
+          
+        </div>
+      </div>
+    </main>
+    <?php include_once (ROOT_PATH . '/include/footer.php'); ?>
+  </body>
+</html>
